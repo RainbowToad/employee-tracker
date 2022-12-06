72575c98bf9067ee35e84889ed60337dd6b31d5e
RainbowToad <chad_spilker@hotmail.com>
2022-12-06 12:05:59 -0800

    initial inquirer questions

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require(`inquirer`);
 
+function initialQuestion() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?"
+      name: "initialQuestion"
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update and employee role" "Quit"]
+    }
+  ]).then(function(answers)){
+    
+  }
+}
+
 
 
 // create the connection to database
